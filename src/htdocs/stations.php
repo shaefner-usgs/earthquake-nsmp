83f3de5a6eb9608e4d9440dbe36ceb86c2ff7125
Scott Haefner <shaefner@usgs.gov>
2017-02-09 15:35:20 -0800

    change page title

--- src/htdocs/stations.php
+++ src/htdocs/stations.php
@@ -3,7 +3,7 @@
 include_once '../conf/config.inc.php'; // app config
 
 if (!isset($TEMPLATE)) {
-  $TITLE = 'National Stong Motion Project';
+  $TITLE = 'NSMP Stations';
   $NAVIGATION = true;
   $HEAD = '
     <link rel="stylesheet" href="/lib/leaflet-0.7.7/leaflet.css" />
