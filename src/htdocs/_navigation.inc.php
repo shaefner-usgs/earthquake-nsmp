f2cdf6a6e4f48aa57ff959e18e1822002c8adf0c
Scott Haefner <shaefner@usgs.gov>
2017-02-10 15:55:32 -0800

    remove unused code; add index.php to overview page

--- src/htdocs/_navigation.inc.php
+++ src/htdocs/_navigation.inc.php
@@ -3,18 +3,9 @@
 $section = $CONFIG['MOUNT_PATH'];
 $url = $_SERVER['REQUEST_URI'];
 
-$matches_index = false;
-if (preg_match("@^$section(/index.php)?$@", $url)) {
-  $matches_index = true;
-}
-$matches_arrays = false;
-if (preg_match("@^$section/arrays(/index.php)?$@", $url)) {
-  $matches_arrays = true;
-}
-
 $NAVIGATION =
   navGroup('NSMP',
-    navItem("$section/", 'Overview') .
+    navItem("$section/index.php", 'Overview') .
     navItem("$section/stations.php", 'Station Map') .
     navItem("$section/arrays/", 'Structural Arrays') .
     navItem("$section/buildings/", 'Buildings') .
