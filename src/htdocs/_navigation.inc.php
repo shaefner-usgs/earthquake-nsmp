f8b0bd693c4405dbde8079e07feb72cc65276729
Scott Haefner <shaefner@usgs.gov>
2017-02-13 21:49:53 -0800

    strip index.php off; use regex match

--- src/htdocs/_navigation.inc.php
+++ src/htdocs/_navigation.inc.php
@@ -3,9 +3,11 @@
 $section = $CONFIG['MOUNT_PATH'];
 $url = $_SERVER['REQUEST_URI'];
 
+$matchesIndex = preg_match("@^$section/$@", $_SERVER['REQUEST_URI']);
+
 $NAVIGATION =
   navGroup('NSMP',
-    navItem("$section/index.php", 'Overview') .
+    navItem("$section/", 'Overview', $matchesIndex) .
     navItem("$section/stations.php", 'Station Map') .
     navItem("$section/arrays/", 'Structural Arrays') .
     navItem("$section/buildings/", 'Buildings') .
