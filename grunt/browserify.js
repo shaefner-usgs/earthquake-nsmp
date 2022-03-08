9e3d53489f7acc2e7a60f0921673715dd9612f4f
Scott Haefner <4175182+shaefner-usgs@users.noreply.github.com>
2022-03-08 23:28:15 +0000

    add automatically polyfilling; comment out timestamp

--- grunt/browserify.js
+++ grunt/browserify.js
@@ -6,7 +6,7 @@ var babelify = require('babelify'),
 
 var browserify = {
   options: {
-    banner: '/* browserified: <%= grunt.template.today("mm-dd-yyyy hh:MM:ss") %> */\n',
+    //banner: '/* browserified: <%= grunt.template.today("mm-dd-yyyy hh:MM:ss") %> */\n',
     browserifyOptions: {
       debug: true, // inline sourcemaps
       paths: [
@@ -20,6 +20,9 @@ var browserify = {
       babelify.configure({
         presets: [
           '@babel/preset-env'
+        ],
+        plugins: [
+          '@babel/plugin-transform-runtime'
         ]
       })
     ]
