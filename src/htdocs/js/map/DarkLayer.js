7502ef73a1433243c22e07a573bf84d19ca0e4b2
Scott Haefner <4175182+shaefner-usgs@users.noreply.github.com>
2022-03-10 20:10:20 +0000

    add esri option; remove hazdev dep; code optimization

--- src/htdocs/js/map/DarkLayer.js
+++ src/htdocs/js/map/DarkLayer.js
@@ -2,33 +2,61 @@
 'use strict';
 
 
-var Util = require('hazdev-webutils/src/util/Util');
-
-
 /**
- * Factory for Dark base layer
+ * Factory for Dark base layer.
  *
+ * @param provider {String} optional; default is 'cartodb'
  * @param options {Object}
- *     L.TileLayer options
+ *     L.tileLayer options
  *
- * @return {L.TileLayer}
+ * @return {L.tileLayer|L.layerGroup}
  */
-var DarkLayer = function (options) {
-  options = Util.extend({
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">' +
-      'OpenStreetMap</a> &copy; <a href="https://cartodb.com/attributions">' +
-      'CartoDB</a>',
-    maxZoom: 19,
-    subdomains: 'abcd'
-  }, options);
+L.DarkLayer = function (provider, options) {
+  var base,
+      labels,
+      providers,
+      url;
 
-  return L.tileLayer(
-    'https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}@2x.png',
-    options
-  );
+  providers = {
+    cartodb: {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">' +
+        'OpenStreetMap</a> &copy; <a href="https://cartodb.com/attributions">' +
+        'CartoDB</a>',
+      maxZoom: 19,
+      subdomains: 'abcd',
+      url: 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}@2x.png'
+    },
+    esri: {
+      attribution: 'Tiles &copy; Esri — Esri, DeLorme, NAVTEQ',
+      maxZoom: 16,
+      subdomains: [
+        'server',
+        'services'
+      ],
+      url: 'https://{s}.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}'
+    }
+  };
+  provider = provider || 'cartodb';
+  options = Object.assign({}, providers[provider], options);
+  url = providers[provider].url;
+  base = L.tileLayer(url, options);
+
+  // ESRI Dark layer doesn't include labels; add them
+  if (provider === 'esri') {
+    labels = L.tileLayer(
+      'https://services.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Dark_Gray_Reference/MapServer/tile/{z}/{y}/{x}'
+    );
+
+    return L.layerGroup([
+      base,
+      labels
+    ]);
+  } else {
+    return base;
+  }
 };
 
 
-L.darkLayer = DarkLayer;
-
-module.exports = DarkLayer;
+L.darkLayer = function () {
+  return new L.DarkLayer();
+};
